b19f23643ec372a34378bbbab6a8d8b561c921ae
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2020-02-03 22:39:35 -0500

    Edge Detecton Test

---
+++ cv_edges.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+img_dir = 'img/'
+
+if __name__ == "__main__":
+    print("Edge Detection")
+
+    im1 = cv.imread(img_dir + 'good_angle_right.jpg', cv.IMREAD_GRAYSCALE)    
+    im2 = cv.imread(img_dir + 'bad_angle_right.JPG', cv.IMREAD_GRAYSCALE)    
+
+
+    edges1 = cv.Canny(im1,100,200)
+    edges2 = cv.Canny(im2,100,200)
+
+
+    cv.namedWindow('Good Case Test')
+    cv.imshow('Good Case Test', edges1)
+
+    cv.namedWindow('Bad Case Test')
+    cv.imshow('Bad Case Test', edges2)
+
+    cv.waitKey(0)
