276c322443ff8a835e7ed002dd2ca819ff7511cc
Jon <jon.esparaz@gmail.com>
2020-02-19 18:23:51 -0500

    servo testing

--- servo_test/servotest.py
+++ servo_test/servotest.py
@@ -1,30 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 17
+servoPINA = 17
+servoPINB = 18
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(servoPINA, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPINB, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+pa = GPIO.PWM(servoPINA, 50) # GPIO 17 for PWM with 50Hz
+pb = GPIO.PWM(servoPINB, 50) # GPIO 18 for PWM with 50Hz
+pa.start(2.5) # Initialization
+pb.start(2.5) # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(5)
+    pa.ChangeDutyCycle(5)
+    pb.ChangeDutyCycle(5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
+    pa.ChangeDutyCycle(7.5)
+    pb.ChangeDutyCycle(7.5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(10)
+    pa.ChangeDutyCycle(10)
+    pb.ChangeDutyCycle(10)
     time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
+    pa.ChangeDutyCycle(12.5)
+    pb.ChangeDutyCycle(12.5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(10)
+    pa.ChangeDutyCycle(10)
+    pb.ChangeDutyCycle(10)
     time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
+    pa.ChangeDutyCycle(7.5)
+    pb.ChangeDutyCycle(7.5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(5)
+    pa.ChangeDutyCycle(5)
+    pb.ChangeDutyCycle(5)
     time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
+    pa.ChangeDutyCycle(2.5)
+    pb.ChangeDutyCycle(2.5)
     time.sleep(0.5)
 except KeyboardInterrupt:
-  p.stop()
+  pa.stop()
+  pb.stop()
   GPIO.cleanup()
