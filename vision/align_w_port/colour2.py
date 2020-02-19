39b462f359e4d242ad396a7f12609da34ab7a348
Jon <jon.esparaz@gmail.com>
2020-02-18 22:57:49 -0500

    Determine relative position

--- vision/align_w_port/colour2.py
+++ vision/align_w_port/colour2.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2 as cv
 from statistics import mean 
 
-image = cv.imread('l6.jpg')
+image = cv.imread('l2.jpg')
 
 blue = ([95,65,0], [165,180,40])
 
@@ -13,13 +13,11 @@ upper = np.array(upper, dtype = "uint8")
 mask = cv.inRange(image, lower, upper)
 output = cv.bitwise_and(image, image, mask = mask)
 
-crop = output[500:1944-500,0:2592]
-
 '''new = cv.resize(crop, (int(2592/3),int(1944/3)))
 cv.imshow("Blue only", new)
 cv.waitKey(0)'''
 
-dst = cv.Canny(crop, 100, 200)
+dst = cv.Canny(output, 100, 200)
     
 cdstP = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
 
@@ -44,36 +42,33 @@ if lines is not None:
             data.append(pt2)
             cv.line(cdstP, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
 
-#Redo below...
+data_final = []
 
-x_pos_final = []
+for i in range(0,len(data),2):
+    temp = (data[i][0] + data[i+1][0])/2
+    data_final.append(temp)
 
-for i in range(0,len(x_pos),4):
-    x_pos_final.append(x_pos[i])
+data_final.sort()
 
-x_pos_final.sort()
-
-print(x_pos_final)
 def get_xvals(sorted_list):
     avg_vals = []
     acc = []
-    for i in range (0,len(sorted_list)-1):
-        test_a = sorted_list[i]
-        test_b = sorted_list[i+1]
+    for i in range (1,len(sorted_list)):
+        test_a = sorted_list[i-1]
+        test_b = sorted_list[i]
 
         test = test_a/test_b
-        print(test)
 
-        if test < 0.96:
+        acc.append(test_a)
+        if test < 0.98:
             temp = mean(acc)
             avg_vals.append(temp)
             acc = []
-        if i == (len(sorted_list)-2):
+        if i == (len(sorted_list)-1):
+            acc.append(test_b)
             temp = mean(acc)
             avg_vals.append(temp)
             acc = []
-        else:
-            acc.append(test_a)
 
     for i in avg_vals:
         x1 = avg_vals[0]
@@ -84,10 +79,10 @@ def get_xvals(sorted_list):
     
     return([x1, x2])
 
-positions = get_xvals(x_pos_final)
+positions = get_xvals(data_final)
 
 def move(a,b):
-    tolerance = 50
+    tolerance = 5
     left = a
     right = 2592 - b
     test = left - right
@@ -98,6 +93,7 @@ def move(a,b):
     else:
         return('ok!')
 
+print(positions)
 print(move(positions[0],positions[1]))
 
 new = cv.resize(cdstP, (int(2592/3),int(1944/3)))
@@ -107,4 +103,4 @@ cv.waitKey(0)
 #sm_image = cv.resize(image, (320,180))
 #sm_output = cv.resize(output, (320,180))
 #cv.imshow("images", np.hstack([sm_image, sm_output]))
-#cv.waitKey(0)
+#cv.waitKey(0)'''
