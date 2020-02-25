9d44f7885092b25f61dc4225e6d407b22f1eb9dd
Jon <jon.esparaz@gmail.com>
2020-02-24 22:58:32 -0500

    High-level sudo code

---
+++ main/main.py
@@ -0,0 +1,27 @@
+'''This is sudo code...
+
+Move to far left from starting position
+
+While port is not seen
+    Take picture
+    Move left
+
+While angle not 90 deg
+    Determine angle
+    Rotate to align with port
+
+While port is not aligned
+    Determine position
+    Move to align with port
+
+Insert into port
+
+Check ToF sensors and camera to ensure we charger has been inserted
+
+Indicate that charger has been inserted
+
+Wait 5 sec
+
+Release charger
+
+Return to starting position'''
