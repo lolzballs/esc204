6e0d899639e6dce61ec0810a235c9953551b8066
Jon <jon.esparaz@gmail.com>
2020-03-06 12:31:59 -0500

    fix subprocess

--- indicators/audio.py
+++ indicators/audio.py
@@ -4,8 +4,8 @@ class Indicator:
     def __init__(self):
         pass
     def located(self):
-        subprocess.call(["mpv", ".mp3"])
+        call(["mpv", ".mp3"])
     def delivered(self):
-        subprocess.call(["mpv", ".mp3"])
+        call(["mpv", ".mp3"])
     def inserted(self):
-        subprocess.call(["mpv", ".mp3"])
+        call(["mpv", ".mp3"])
