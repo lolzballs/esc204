cef03a8e18e3f072d30071a2fa4ced00198473df
Jon Esparaz <jon.esparaz@gmail.com>
2020-03-12 18:05:39 -0400

    create class for testing

--- main/milestone2.py
+++ main/milestone2.py
@@ -1,3 +1,19 @@
 import camera
 import kinematics
 
+class milestone2:
+
+    def __init__(self):
+        self.cam = camera.SetCam()
+        self.mot = kinematics.Motion()
+
+    def main(self):
+        y = input('distance')
+
+        move = self.cam.determine_move(y)
+
+        if move[0] == -1:
+            self.mot.step_left()
+        
+        if move[0] == 1:
+            self.mot.step_right()
